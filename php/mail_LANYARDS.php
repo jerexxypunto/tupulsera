e8856791c04c417f902bd267eaf3eabc9738160d
Jeremias <jerexxypunto@gmail.com>
2022-03-22 11:36:39 -0300

    mend

--- php/mail_LANYARDS.php
+++ php/mail_LANYARDS.php
@@ -1,34 +1,45 @@
 <?php
-   $ancho = $_POST['color'];
-   $impresion = $_POST['color_frase'];
-   $cantidad = $_POST['cantidad'];
-   $nombre = $_POST['name'];
-   $email  = $_POST['email'];
-   $telefono  = $_POST['phone'];
-   $msj = $_POST['comments'];
+  if( !empty($_POST['color']) && 
+  	  !empty($_POST['color_frase']) &&
+	  !empty($_POST['cantidad']) && 
+	  !empty($_POST['name']) && 
+	  !empty($_POST['email']) && 
+	  !empty($_POST['phone']) &&
+	  !empty($_POST['comments'])
+	){
+	   	$ancho = $_POST['color'];
+		$impresion = $_POST['color_frase'];
+		$cantidad = $_POST['cantidad'];
+		$nombre = $_POST['name'];
+		$email  = $_POST['email'];
+		$telefono  = $_POST['phone'];
+		$msj = $_POST['comments'];
 
-	$cabeceras = 'MIME-Version: 1.0' . "\r\n";
-	$cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-	$to = 'jerexxypunto@gmail.com';
-	$subject = "Pedido: Layand";
-	$message = "<html>".
-				"<h2>Tiene un pedido de layard</h2>".
-				"<h3>Datos de contacto:</h3>".
-				"<p><b>Nombre:</b> $nombre</p>".
-				"<p><b>Email:</b> $email</p>".
-				"<p><b>Telefono:</b> $telefono</p>".
-				"<h3>Datos del pedido:</h3>".
-				"<p><b>Ancho:</b> $ancho</p>".
-				"<p><b>Impresión:</b> $impresion</p>".
-				"<p><b>Cantidad:</b> $cantidad</p>".
-				"<h3>Comentarios:</h3>".
-				"<p>$msj</p>".
-			    "</html>";
-	$message = utf8_decode($message);
-	
-	if(mail(  $to, $subject, $message, $cabeceras)){
-		header('Location:../gracias.html');
-	}else{
-		echo '¡ups ha ocurrido un error al enviar el mensaje!';
-	}
+			$cabeceras = 'MIME-Version: 1.0' . "\r\n";
+			$cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+			$to = 'jerexxypunto@gmail.com';
+			$subject = "Pedido: Layand";
+			$message = "<html>".
+						"<h2>Tiene un pedido de layard</h2>".
+						"<h3>Datos de contacto:</h3>".
+						"<p><b>Nombre:</b> $nombre</p>".
+						"<p><b>Email:</b> $email</p>".
+						"<p><b>Telefono:</b> $telefono</p>".
+						"<h3>Datos del pedido:</h3>".
+						"<p><b>Ancho:</b> $ancho</p>".
+						"<p><b>Impresión:</b> $impresion</p>".
+						"<p><b>Cantidad:</b> $cantidad</p>".
+						"<h3>Comentarios:</h3>".
+						"<p>$msj</p>".
+						"</html>";
+			$message = utf8_decode($message);
+			
+			if(mail(  $to, $subject, $message, $cabeceras)){
+				header('Location:../gracias.html');
+			}else{
+				echo '¡ups ha ocurrido un error al enviar el mensaje!';
+			}
+  }else{
+	  echo '¡ups! no lleno todos los campos';
+  }
 ?>
